b77f62bebe4a2d57cdffc77046e7c01674ae74c8
ClementOLIVIER <clementolivier.co@gmail.com>
2023-03-14 16:27:39 +0100

    Home Page v.1

--- pages/index.vue
+++ pages/index.vue
@@ -2,53 +2,62 @@
   <!-- Landing Page-->
   <section
     class="
-      flex flex-row items-center justify-between
       bg-gradient-to-r from-primary-400 to-primary-600
-      bg-repeat bg-cover bg-center
       min-h-screen
+      flex items-center justify-center
     "
   >
     <!-- TODO: Check hero in https://codewithsadee.github.io/portfolio/# for background image -->
-    <!-- Hero Content -->
-    <div class="hero-content text-left text-white">
-      <h2 class="hero-title">
-        <span>Hello I'm</span>
-        <strong>Ethan Ward</strong> Web Developer from New York
-      </h2>
-
-      <p class="hero-text">
-        Adipisicing elit. Neque nulla voluptatum eos repellat deserunt nesciunt tempore porro totam.
-      </p>
-
-      <div class="btn-group flex justify-start">
-        <a href="#contact" class="btn btn-primary blue">Get a Quote</a>
-
-        <a href="#about" class="btn">About Me</a>
+    <div class="grid grid-cols-2 gap-4 mx-10">
+      <!-- Hero Content -->
+      <div class="text-left text-white">
+        <div class="flex flex-col gap-2">
+          <span>Hello I'm </span>
+          <strong>Clément OLIVIER</strong>
+          <span>Data Scientist, Product Designer & Full Stack Developer</span>
+          <p class="mt-4">
+            Adipisicing elit. Neque nulla voluptatum eos repellat deserunt nesciunt tempore porro totam. Adipisicing elit. Neque nulla voluptatum eos repellat deserunt nesciunt tempore porro totam. Adipisicing elit. Neque nulla voluptatum eos repellat deserunt nesciunt tempore porro totam.
+          </p>
+        </div>
+        <div class="flex justify-center items-center mt-4">
+          <NuxtLink
+            to="/contact"
+            class="
+                p-2
+                text-white rounded-2xl
+                ring-2 ring-white
+                transition-colors duration-200
+                hover:ring-yellow-200
+              "
+          >
+            Hire me
+          </NuxtLink>
+        </div>
       </div>
-    </div>
 
-    <!-- Hero Banner -->
-    <div class="relative">
-      <div>
-        <img
-          src="~/assets/images/hero-picture.png"
-          loading="lazy"
-          class="img-cover object-contain w-96"
+      <!-- Hero Banner -->
+      <div class="relative flex justify-center items-center">
+        <div>
+          <img
+            src="~/assets/images/hero-picture.png"
+            loading="lazy"
+            class="img-cover object-contain w-96"
+          >
+        </div>
+        <div
+          class="
+              absolute flex items-center gap-3 bg-white rounded-sm p-2
+              top-2 left-2
+            "
         >
-      </div>
-      <div
-        class="
-            absolute flex items-center gap-3 bg-white rounded-sm p-2
-            top-2 left-2
-          "
-      >
-        <p class="text-cyan-200">
-          12
-        </p>
+          <p class="text-cyan-200">
+            12
+          </p>
 
-        <p class="font-semibold">
-          Years of Success
-        </p>
+          <p class="font-semibold">
+            Years of Success
+          </p>
+        </div>
       </div>
     </div>
   </section>
