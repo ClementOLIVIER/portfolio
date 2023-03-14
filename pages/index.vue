ce66bbb295b9d99010633ba648037434405eaf5d
ClementOLIVIER <clementolivier.co@gmail.com>
2023-03-14 18:00:33 +0100

    Integrate background

--- pages/index.vue
+++ pages/index.vue
@@ -2,61 +2,68 @@
   <!-- Landing Page-->
   <section
     class="
-      bg-gradient-to-r from-primary-400 to-primary-600
+      bg-[url('~/assets/images/landing-page-background.png')]
+      bg-cover bg-center bg-no-repeat
       min-h-screen
       flex items-center justify-center
     "
   >
-    <!-- TODO: Check hero in https://codewithsadee.github.io/portfolio/# for background image -->
-    <div class="grid grid-cols-2 gap-4 mx-10">
-      <!-- Hero Content -->
-      <div class="text-left text-white">
-        <div class="flex flex-col gap-2">
-          <span>Hello I'm </span>
-          <strong>Clément OLIVIER</strong>
-          <span>Data Scientist, Product Designer & Full Stack Developer</span>
-          <p class="mt-4">
-            Adipisicing elit. Neque nulla voluptatum eos repellat deserunt nesciunt tempore porro totam. Adipisicing elit. Neque nulla voluptatum eos repellat deserunt nesciunt tempore porro totam. Adipisicing elit. Neque nulla voluptatum eos repellat deserunt nesciunt tempore porro totam.
-          </p>
+    <div>
+      <!-- TODO: Check hero in https://codewithsadee.github.io/portfolio/# for background image -->
+      <div class="grid grid-cols-2 gap-4 mx-10">
+        <!-- Hero Content -->
+        <div class="text-left text-white backdrop-blur-sm">
+          <div class="flex flex-col gap-2">
+            <span>Hello I'm </span>
+            <strong>Clément OLIVIER</strong>
+            <span>Data Scientist, Product Designer & Full Stack Developer</span>
+            <p class="mt-4">
+              Adipisicing elit. Neque nulla voluptatum eos repellat deserunt nesciunt tempore porro totam. Adipisicing elit. Neque nulla voluptatum eos repellat deserunt nesciunt tempore porro totam. Adipisicing elit. Neque nulla voluptatum eos repellat deserunt nesciunt tempore porro totam.
+            </p>
+          </div>
+          <div class="flex justify-center items-center mt-4">
+            <NuxtLink
+              to="/contact"
+              class="
+                  p-2
+                  text-white rounded-2xl
+                  ring-2 ring-white
+                  transition-colors duration-200
+                  hover:ring-yellow-200
+                "
+            >
+              Hire me
+            </NuxtLink>
+          </div>
         </div>
-        <div class="flex justify-center items-center mt-4">
-          <NuxtLink
-            to="/contact"
+
+        <!-- Hero Banner -->
+        <div class="relative flex justify-center items-center">
+          <div>
+            <img
+              src="~/assets/images/hero-picture.png"
+              loading="lazy"
+              class="
+                img-cover object-contain w-96
+                rounded-xl
+                border-4 border-white
+              "
+            >
+          </div>
+          <div
             class="
-                p-2
-                text-white rounded-2xl
-                ring-2 ring-white
-                transition-colors duration-200
-                hover:ring-yellow-200
+                absolute flex items-center gap-3 bg-white rounded-sm p-2
+                top-2 left-2
               "
           >
-            Hire me
-          </NuxtLink>
-        </div>
-      </div>
+            <p class="text-black font-bold">
+              10
+            </p>
 
-      <!-- Hero Banner -->
-      <div class="relative flex justify-center items-center">
-        <div>
-          <img
-            src="~/assets/images/hero-picture.png"
-            loading="lazy"
-            class="img-cover object-contain w-96"
-          >
-        </div>
-        <div
-          class="
-              absolute flex items-center gap-3 bg-white rounded-sm p-2
-              top-2 left-2
-            "
-        >
-          <p class="text-cyan-200">
-            12
-          </p>
-
-          <p class="font-semibold">
-            Years of Success
-          </p>
+            <p class="font-semibold">
+              Years of Experience
+            </p>
+          </div>
         </div>
       </div>
     </div>
